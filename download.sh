d44ddd04dcf300599375efcf41f6190f654c56a5
VuHuu <162543040+Vucore@users.noreply.github.com>
2025-03-15 23:59:53 +0700

    Add Streamlit app with download.sh

--- download.sh
+++ download.sh
@@ -1,3 +1,9 @@
-#!/bin/bash
+!/bin/bash
+
 pip install gdown
-gdown --id 1bWoBSU5Hq1wNQUYuP_BS565P4C3Rr7Bp -O filenames.pkl
+
+FILENAMES_URL="https://drive.google.com/uc?id=1bWoBSU5Hq1wNQUYuP_BS565P4C3Rr7Bp"  
+IMAGE_FEATURES_URL="https://drive.google.com/uc?id=1iq7xKxz_LUZDIT0wCix0fIr4KmrjgjK1" 
+
+gdown "$FILENAMES_URL" -O filenames.pkl
+gdown "$IMAGE_FEATURES_URL" -O image_features.pkl
